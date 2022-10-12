740973c4e3d8a7e2df80d20f3b3dc3486523a75f
nicklin-sjsu <nicklin1219@gmail.com>
2022-10-12 00:32:32 -0700

    Complete s3 upload

--- app.js
+++ app.js
@@ -143,24 +143,40 @@ app.get('/sso', checkNotAuthenticated, function (req, res) {
 });
 
 app.post('/upload_file', checkAuthenticated, function (req, res) {
-    console.log(req);
     var user_id = req.user.id;
     var form = new multiparty.Form();
     form.parse(req, (err, fields, files) => {
         var fields_list = Object.entries(fields);
         var files_list = Object.entries(files);
-        var file = files_list['file'].file_name;
-        var file_name = file.originalFilename
+        var file = files_list['file'];
+        var file_name = fields_list['file_name'];
+        var description = fields_list['description'];
 
-        var params = {
-            Body: files_list['file'],
-            Bucket: 'fileweb-aws-s3',
-            Key: user_id + "/" + file_name;
-        };
-        s3.putObject(params, function (err, data) {
-            if (err) console.log(err, err.stack);
-            else console.log(data);
-
+        var sql1 = mysql.format('SELECT * FROM files WHERE user_id = ? AND file_key = ?', [user_id, file_name]);
+        con.query(sql1, function (err, result) {
+            if (result.length > 0) {
+                res.send({ 'code': 400, 'message': 'file with same name exists, please use another name' });
+            } else {
+                var params = {
+                    Body: files_list['file'],
+                    Bucket: 'fileweb-aws-s3',
+                    Key: user_id + "/" + file_name
+                };
+                s3.putObject(params, function (err, data) {
+                    if (err) console.log(err, err.stack);
+                    else {
+                        var sql = mysql.format('INSERT INTO files (user_id, file_key, description) VALUES (?, ?, ?)',
+                            [user_id, file_name, description]);
+                        con.query(sql, function (err, result) {
+                            if (err) {
+                                res.send({ 'code': 400, 'message': 'information error' });
+                            } else {
+                                res.send({ 'code': 200, 'message': 'upload successfully' });
+                            }
+                        });
+                    }
+                });
+            }
         });
     });
     
